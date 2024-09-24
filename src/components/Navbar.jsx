10e525f4a5a3052e1e03cd0ebf31c5f4dca80a67
Sharan505 <sharan27505@gmail.com>
2024-09-24 09:06:49 +0530

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import './component.css';
  function Navbar(){
     return(
-        <div className="container fixed-top">
+        <div className="container fixed-top" style={{backgroundColor:"black"}}>
             <div className="row">
                 <ul className="col nav d-flex justify-content-end gap-5 h5 mt-4 mb-sm-3 mb-md-5" style={{ fontSize: 'clamp(.5rem, 3vw, 1rem)' }}>
                     <li><a className="text-white text-decoration-none" href="#About">ABOUT</a></li>
